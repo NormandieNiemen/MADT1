a0a86a625bbcc4fe3ac2f3809363ef79328f0f2f
roseofwinds <artsemhalkin@gmail.com>
2021-10-11 01:28:28 +0300

    Revert "Button "Buy Crypto" with onClick EventListener added."
    This reverts commit 7098855f

--- app/src/main/java/com/example/madt1artsemhalkin/MainActivity.java
+++ app/src/main/java/com/example/madt1artsemhalkin/MainActivity.java
@@ -4,21 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textBuyCrypto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.textBuyCrypto = findViewById(R.id.textBuyCrypto);
-    }
-
-    public void buyCryptoClick(View view) {
-        this.textBuyCrypto.setText("You have successfully bought 25DOT!");
     }
 
     public void changeTextColorClick(View view) {
