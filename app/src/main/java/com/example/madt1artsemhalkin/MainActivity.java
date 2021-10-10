7098855fffab0315884aa850378504814ece6841
roseofwinds <artsemhalkin@gmail.com>
2021-10-11 01:05:10 +0300

    Button "Buy Crypto" with onClick EventListener added.

--- app/src/main/java/com/example/madt1artsemhalkin/MainActivity.java
+++ app/src/main/java/com/example/madt1artsemhalkin/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.madt1artsemhalkin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textBuyCrypto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.textBuyCrypto = findViewById(R.id.textBuyCrypto);
+    }
+
+    public void buyCryptoClick(View view) {
+        this.textBuyCrypto.setText("You have successfully bought 25DOT!");
     }
 }
